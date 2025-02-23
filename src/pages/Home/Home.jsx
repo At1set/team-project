d8ca2cb7361f818010924194021782324b51b06d
At1set <iliya9874329@gmail.com>
2025-02-23 20:51:23 +0300

    some changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <section className="HomePage page-root">
-      <h1>😎 Главная страница </h1>
+      <h1>🤩 Главная страница 🤩</h1>
       <span className="hidden-text">.</span>
     </section>
   )
